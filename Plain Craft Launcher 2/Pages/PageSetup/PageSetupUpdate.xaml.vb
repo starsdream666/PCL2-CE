6e35ac48c5bfed497e9a679964e9646b80dcf664
Pigeon0v0 <60414767+Pigeon0v0@users.noreply.github.com>
2025-11-22 23:19:46 +0800

    feat: 识别配置项

--- Plain Craft Launcher 2/Pages/PageSetup/PageSetupUpdate.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageSetup/PageSetupUpdate.xaml.vb
@@ -1,21 +1,18 @@
-﻿Public Class PageSetupUpdate
+﻿Imports PCL.Core.App
 
-    Private IsLoad As Boolean = False
-
+Public Class PageSetupUpdate
     Private Sub PageSetupUpdate_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-
-        '非重复加载部分
-        If IsLoad Then Return
-        IsLoad = True
-
-        AniControlEnabled += 1
-        AniControlEnabled -= 1
-
+        ComboSystemUpdateChannel.SelectedIndex = Config.System.UpdateBranch
+        ComboSystemUpdateMode.SelectedIndex = Config.System.UpdateSolution
     End Sub
     
     Private Sub BtnUpdate_Click(sender As Object, e As EventArgs) Handles BtnUpdate.Click
         FrmMain.BtnExtraUpdateRestart.ShowRefresh()
         FrmMain.BtnExtraUpdateRestart.Ribble()
+    End Sub
+    
+    Private Sub BtnChangelogDetail_Click(sender As Object, e As EventArgs) Handles BtnChangelogDetail.Click
+        
     End Sub
     
 End Class
